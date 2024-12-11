78ef60ffd84e575c16d5770ea1456aa01fa14511
kemkum53 <kondakci.k@gmail.com>
2024-12-12 01:34:04 +0300

    selectors moved to global varriables

--- wp_message/WhatsApp_Selenium.py
+++ wp_message/WhatsApp_Selenium.py
@@ -13,40 +13,49 @@ import argparse
 import asyncio
 import sys
 
+EN_NEW_CHAT_ELEMENT = [By.CSS_SELECTOR, "div[aria-label=\'New chat\']"]
+LOADING_BAR = [By.TAG_NAME, "progress"]
+EN_LOGIN_QR_CODE_ELEMENT = [By.CSS_SELECTOR, "canvas[aria-label=\'Scan this QR code to link a device!\']"]
+EN_LOGIN_WITH_PHONE_ELEMENT = [By.XPATH, "//span[contains(., 'Log in with phone number')]"]
+EN_LOGIN_TYPE_PHONE_NUMBER_ELEMENT = [By.CSS_SELECTOR, "input[aria-label=\'Type your phone number.\']"]
+EN_LOGIN_NEXT_BUTTON_ELEMENT = [By.XPATH, "//div[text()=\'Next\']"]
+EN_LOGIN_CODE_WAIT_ELEMENT = [By.CSS_SELECTOR, "div[aria-label=\'Enter code on phone:\'] span"]
+LOGIN_CODE_ELEMENT = [By.CSS_SELECTOR, "div[data-link-code]"]
+SEND_MESSAGE_PHONE_INPUT_ELEMENT = {By.CSS_SELECTOR, "div[contenteditable=\'true\']"}
+EN_SEND_MESSAGE_TEXT_INPUT_ELEMENT = {By.CSS_SELECTOR, "div[aria-placeholder=\'Type a message\']"}
 
 class WhatsApp_Selenium:
-    def __init__(self, chrome_data_dir:str='./chrome-data'):
+    def __init__(self, chrome_data_dir:str=f"{os.getenv('LOCALAPPDATA')}\\Google\\Chrome\\User Data\\Profile 1"):
         self.hide_browser = False
         self.browser_open = False
         self.chrome_data_dir = chrome_data_dir
         self.driver = None
 
     def start_browser(self):
-        '''Start browser and open WhatsApp Web.'''
+        """Start browser and open WhatsApp Web."""
         if not os.path.exists(self.chrome_data_dir):
             os.mkdir(self.chrome_data_dir)
-            
         chrome_options = Options()
-        chrome_options.add_argument(f'--user-data-dir={self.chrome_data_dir}')  # Keep session infos
+        chrome_options.add_argument(f"--user-data-dir={self.chrome_data_dir}")  # Keep session infos
         
         # Set visibility
         if self.hide_browser:
-            chrome_options.add_argument('--headless')
-            
+            chrome_options.add_argument("--headless")
+        
         # Start chrome
         service = Service()
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
-        self.driver.get('https://web.whatsapp.com')
+        self.driver.get("https://web.whatsapp.com")
 
     def close_browser(self):
-        '''Close browser'''
+        """Close browser"""
         if self.driver:
             self.driver.quit()  
 
     def wait_qr(self):
-        '''Check qr code is visible'''
+        """Check qr code is visible"""
         try:
-            canvas_element = self.driver.find_element(By.CSS_SELECTOR, 'canvas[aria-label=\'Scan this QR code to link a device!\']')
+            canvas_element = self.driver.find_element(EN_LOGIN_QR_CODE_ELEMENT[0], EN_LOGIN_QR_CODE_ELEMENT[1])
             if canvas_element.is_displayed():
                 return False
             else:
@@ -55,18 +64,18 @@ class WhatsApp_Selenium:
             return True
 
     def wait_chat_screen(self) -> bool:
-        '''Wait for chat screen to load
+        """Wait for chat screen to load
             For while loop
             -> True (Chat screen not loaded)
-            -> False (Chat screen loaded)'''
+            -> False (Chat screen loaded)"""
         try:
-            self.driver.find_element(By.CSS_SELECTOR, 'div[aria-label=\'New chat\']')
+            self.driver.find_element(EN_NEW_CHAT_ELEMENT[0], EN_NEW_CHAT_ELEMENT[1])
             return False
         except:
             return True
 
     async def wait_element(self, by:By, search_value:str, time_limit:int, ex_code:int):
-        '''Wait for element to load with timer'''
+        """Wait for element to load with timer"""
         counter = 0
         while counter < time_limit:
             try:
@@ -77,17 +86,17 @@ class WhatsApp_Selenium:
             except:
                 time.sleep(1)
                 
-        raise TimeOutException('Element dosen\'t found...', ex_code) 
+        raise TimeOutException("Element dosen\'t found...", ex_code) 
 
     async def session_status(self):
-        '''Check active sessions
+        """Check active sessions
             -> False (not logged in)
-            -> True (logged in)'''
+            -> True (logged in)"""
         try:
             # Check data folder
             if not os.path.exists(self.chrome_data_dir):
                 return False
-            chat = await self.wait_element(By.CSS_SELECTOR, 'div[aria-label=\'New chat\']', 5, 1010)
+            chat = await self.wait_element(EN_NEW_CHAT_ELEMENT[0], EN_NEW_CHAT_ELEMENT[1], 5, 1010)
             return True
         except TimeOutException as e:
             return False
@@ -95,10 +104,10 @@ class WhatsApp_Selenium:
             raise e
 
     async def wait_loader(self):
-        '''Wait for first loading page'''
+        """Wait for first loading page"""
         while True:
             try:
-                loading_bar = self.driver.find_element(By.TAG_NAME, 'progress')
+                loading_bar = self.driver.find_element(LOADING_BAR[0], LOADING_BAR[1])
                 time.sleep(1)
             except:
                 break
@@ -108,68 +117,68 @@ class WhatsApp_Selenium:
         try:
             self.start_browser() # Open browser
             
-            print(f'{Fore.BLUE}[INF]{Fore.RESET} Waiting for page to load...')
+            print(f"{Fore.BLUE}[INF]{Fore.RESET} Waiting for page to load...")
             await self.wait_loader()
             
-            print(f'{Fore.BLUE}[INF]{Fore.RESET} Checking login status...')
+            print(f"{Fore.BLUE}[INF]{Fore.RESET} Checking login status...")
             if await self.session_status():
-                print(f'{Fore.YELLOW}[WAR]{Fore.RESET} Already logged in.')
+                print(f"{Fore.YELLOW}[WAR]{Fore.RESET} Already logged in.")
                 return
             
-            print(f'{Fore.BLUE}[INF]{Fore.RESET} Logging in with phone number...')
+            print(f"{Fore.BLUE}[INF]{Fore.RESET} Logging in with phone number...")
             # Wait for qr
             qr_code_counter = 0
             while self.wait_qr():
                 qr_code_counter += 1
                 time.sleep(1)
                 if qr_code_counter >= 10:
-                    raise TimeOutException('Page cannot loaded. Please try again...', 1001)                
+                    raise TimeOutException("Page cannot loaded. Please try again...", 1001)                
             
             # Find and click 'Link with phone number' area
-            button = await self.wait_element(By.XPATH, '//span[text()=\'Link with phone number\']', 5, 1002)
+            button = await self.wait_element(EN_LOGIN_WITH_PHONE_ELEMENT[0], EN_LOGIN_WITH_PHONE_ELEMENT[1], 5, 1002)
             button.click()
             time.sleep(1)
             
             # Find phone number input
-            input_field = await self.wait_element(By.CSS_SELECTOR, 'input[aria-label=\'Type your phone number.\']', 5, 1003)
+            input_field = await self.wait_element(EN_LOGIN_TYPE_PHONE_NUMBER_ELEMENT[0], EN_LOGIN_TYPE_PHONE_NUMBER_ELEMENT[1], 5, 1003)
             input_field.send_keys(number)  # Fill with number
             
             # Click 'Next' button
-            next_button = await self.wait_element(By.XPATH, '//div[text()=\'Next\']', 5, 1004)
+            next_button = await self.wait_element(EN_LOGIN_NEXT_BUTTON_ELEMENT[0], EN_LOGIN_NEXT_BUTTON_ELEMENT[1], 5, 1004)
             next_button.click()
             
-            print(f'{Fore.BLUE}[INF]{Fore.RESET} Watigin for login code...')
+            print(f"{Fore.BLUE}[INF]{Fore.RESET} Watigin for login code...")
             # Wait for code spans
             code_counter = 0
             while True:
                 code_counter += 1
                 try:
-                    spans = await self.wait_element(By.CSS_SELECTOR, 'div[aria-label=\'Enter code on phone:\'] span', 1, 1005)
-                    link_code_element = self.driver.find_element(By.CSS_SELECTOR, 'div[data-link-code]')
+                    spans = await self.wait_element(EN_LOGIN_CODE_WAIT_ELEMENT[0], EN_LOGIN_CODE_WAIT_ELEMENT[1], 1, 1005)
+                    link_code_element = self.driver.find_element(LOGIN_CODE_ELEMENT[0], LOGIN_CODE_ELEMENT[1])
                     break
                 except:
                     time.sleep(1)
                 finally:
                     if code_counter >= 5:
-                        raise TimeOutException('Code not found...', 1006)
+                        raise TimeOutException("Code not found...", 1006)
             
             # If code spans found get the code
-            link_code = link_code_element.get_attribute('data-link-code')
-            link_code_parts = link_code.split(',')
-            formatted_code = ''.join(link_code_parts[:4]) + '-' + ''.join(link_code_parts[4:])
+            link_code = link_code_element.get_attribute("data-link-code")
+            link_code_parts = link_code.split(",")
+            formatted_code = "".join(link_code_parts[:4]) + "-" + "".join(link_code_parts[4:])
             
             code_counter = 3 * 60 # Set timer 3mins
-            print(f'{Fore.GREEN}To log in, please enter this code via WhatsApp:{Fore.RESET} {Fore.BLUE + formatted_code + Fore.RESET}')
+            print(f"{Fore.GREEN}To log in, please enter this code via WhatsApp:{Fore.RESET} {Fore.BLUE + formatted_code + Fore.RESET}")
             while True:
                 mins, secs = divmod(code_counter, 60)
-                timer = f'{mins:02d}:{secs:02d}'
-                print(f'Remaining Time: {Fore.RED + timer + Fore.RESET}', end='\r')
+                timer = f"{mins:02d}:{secs:02d}"
+                print(f"Remaining Time: {Fore.RED + timer + Fore.RESET}", end="\r")
                 time.sleep(1)
                 code_counter -= 1
                 if code_counter <= 0:
-                    raise TimeOutException('Code expired.', 1006)
+                    raise TimeOutException("Code expired.", 1006)
                 elif not self.wait_chat_screen():
-                    print(Fore.GREEN + '[INF]' + Fore.RESET + f' - Loggin success.')
+                    print(Fore.GREEN + "[INF]" + Fore.RESET + f" - Loggin success.")
                     break
         except WebDriverException as e:
             raise BrowserClosedException(1000)
@@ -179,12 +188,13 @@ class WhatsApp_Selenium:
             self.close_browser()
 
     def logout(self):
-        '''Logout function, Clear cache and close session'''
+        """Logout function, Clear cache and close session"""
         try:
+            #TODO logout with using whatsapp logout
             # Delete data_dir
             if os.path.exists(self.chrome_data_dir):
                 shutil.rmtree(self.chrome_data_dir)
-            print(Fore.GREEN + '[SUC]' + Fore.RESET + f'Logged out.')
+            print(Fore.GREEN + "[SUC]" + Fore.RESET + f"Logged out.")
         except Exception as e:
             print(str(e))
     
@@ -192,33 +202,34 @@ class WhatsApp_Selenium:
         try:
             self.start_browser() # Open browser
             
-            print(f'{Fore.BLUE}[INF]{Fore.RESET} Waiting for page to load...')
+            print(f"{Fore.BLUE}[INF]{Fore.RESET} Waiting for page to load...")
             await self.wait_loader()
             
             if not await self.session_status():
-                print(f'{Fore.RED}[ERR]{Fore.RESET} Please login first.')
+                print(f"{Fore.RED}[ERR]{Fore.RESET} Please login first.")
                 return
             
             # Wait loading page
-            new_chat_button = await self.wait_element(By.CSS_SELECTOR, 'div[aria-label=\'New chat\']', 5, 1011)
+            new_chat_button = await self.wait_element(EN_NEW_CHAT_ELEMENT[0], EN_NEW_CHAT_ELEMENT[1], 5, 1011)
             time.sleep(1)
             new_chat_button.click()
             time.sleep(1)
             
+            print(Fore.BLUE + "[INF]" + Fore.RESET + f"Messages sending...")
             for number in numbers:
-                number_input_field = await self.wait_element(By.CSS_SELECTOR, 'div[contenteditable=\'true\']', 5, 1012)
+                number_input_field = await self.wait_element(SEND_MESSAGE_PHONE_INPUT_ELEMENT[0], SEND_MESSAGE_PHONE_INPUT_ELEMENT[1], 5, 1012)
                 number_input_field.send_keys(number)
                 time.sleep(1)
                 number_input_field.send_keys(Keys.ENTER)
                 time.sleep(1)
                 
-                message_input_field = await self.wait_element(By.CSS_SELECTOR, 'div[aria-placeholder=\'Type a message\']', 5, 1013)
+                message_input_field = await self.wait_element(EN_SEND_MESSAGE_TEXT_INPUT_ELEMENT[0], EN_SEND_MESSAGE_TEXT_INPUT_ELEMENT[1], 5, 1013)
                 message_input_field.send_keys(message)
                 time.sleep(1)
                 message_input_field.send_keys(Keys.ENTER)
                 time.sleep(1)
                 
-                print(Fore.GREEN + '[SUC]' + Fore.RESET + f'Message sended to: {number}')
+                print(Fore.GREEN + "[SUC]" + Fore.RESET + f"Message sended to: {number}")
                 time.sleep(1)
         except WebDriverException as e:
             raise BrowserClosedException(1010)
@@ -230,13 +241,13 @@ class WhatsApp_Selenium:
     async def is_logged_in(self):
         try:
             self.start_browser() # Open browser
-            print(f'{Fore.BLUE}[INF]{Fore.RESET} Waiting for page to load...')
+            print(f"{Fore.BLUE}[INF]{Fore.RESET} Waiting for page to load...")
             await self.wait_loader()
             
             if not await self.session_status():
-                print(f'{Fore.BLUE}[INF]{Fore.RESET} {Fore.RED}Not logged in.{Fore.RESET}')
+                print(f"{Fore.BLUE}[INF]{Fore.RESET} {Fore.RED}Not logged in.{Fore.RESET}")
             else:
-                print(f'{Fore.BLUE}[INF]{Fore.RESET} {Fore.GREEN}Already logged in.{Fore.RESET}')
+                print(f"{Fore.BLUE}[INF]{Fore.RESET} {Fore.GREEN}Already logged in.{Fore.RESET}")
         except WebDriverException as e:
             raise BrowserClosedException(1020)
         except Exception as e:
@@ -250,13 +261,13 @@ class WhatsApp_Selenium:
 async def main():
     try:
         # Argparse to handle command line inputs
-        parser = argparse.ArgumentParser(description='Send WhatsApp messages using Selenium (Chrome).')
-        parser.add_argument('--logout', action='store_true', help='Logout from WhatsApp Web and clear session')
-        parser.add_argument('--login', type=str, help='Login with phone number. Please write phone number without country code')
-        parser.add_argument('--session', action='store_true', help='Check active session')
-        parser.add_argument('--hide', action='store_true', help=f'Hide browser. {Fore.RED}[WAR] Do not use when logging in!!{Fore.RESET}')
-        parser.add_argument('--numbers', nargs='+', required=False, help='List of phone numbers to send the message to')
-        parser.add_argument('--message', type=str, required=False, help='Message to be sent')
+        parser = argparse.ArgumentParser(description="Send WhatsApp messages using Selenium (Chrome).")
+        parser.add_argument("--logout", action="store_true", help="Logout from WhatsApp Web and clear session")
+        parser.add_argument("--login", type=str, help="Login with phone number. Please write phone number without country code")
+        parser.add_argument("--session", action="store_true", help="Check active session")
+        parser.add_argument("--hide", action="store_true", help=f"Hide browser. {Fore.RED}[WAR] Do not use when logging in!!{Fore.RESET}")
+        parser.add_argument("--numbers", nargs="+", required=False, help="List of phone numbers to send the message to")
+        parser.add_argument("--message", type=str, required=False, help="Message to be sent")
         args = parser.parse_args()
         
         bot = WhatsApp_Selenium()
@@ -288,6 +299,6 @@ async def main():
         parser.print_help()
         return
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
